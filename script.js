76732f37535cee7221e7eecfdc88f551eb5ea5e2
Danilo Herrera <daniloherrera1702@gmail.com>
2025-12-30 18:38:26 -0500

    input user added with console log

---
+++ script.js
@@ -0,0 +1,9 @@
+        function fun(){
+            alert("FUCK YOU!")
+        }
+
+        function getValue(){
+            var val = document.getElementById("something").value
+            alert("the user wrote: " + val)
+            console.log(val)
+        }
