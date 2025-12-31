4ba3e62d51e77d120ae8da0644d60bea42877383
Danilo Herrera <daniloherrera1702@gmail.com>
2025-12-31 16:30:16 -0500

    we added a local storege and reload display of notes

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-        function fun(){
-            alert("FUCK YOU!")
-        }
+const noteForm = document.getElementById('note-form');
+const noteInput = document.getElementById('note-input');
+const notesList = document.getElementById('notes-list');
 
-        function getValue(){
-            var val = document.getElementById("something").value
-            alert("the user wrote: " + val)
-            console.log(val)
-        }
+// 1. initialize the arry form localstorge or empty
+let notesStorage = JSON.parse(localStorage.getItem('notes')) || [];
+console.log('noteStorage: ' + notesStorage);
+
+// Function to display the notes
+function displayNotes() {
+    notesList.innerHTML = ''; //clean the page
+    notesStorage.forEach(noteText => {
+        const li = document.createElement('li');
+        li.textContent = noteText;
+        notesList.appendChild(li);
+    })
+}
+
+// Function to add notes
+function saveNotes () {
+    //convert the array to a JSON string before saving
+    localStorage.setItem('notes', JSON.stringify(notesStorage));
+    displayNotes();
+}
+
+// Handle form submission
+noteForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const newNote = noteInput.value.trim();
+
+    if (newNote) {
+        notesStorage.push(newNote); // add the note to the array
+        saveNotes(); // save updated array
+        noteInput.value = '';
+    }
+});
+
+// Diplay notes when the page loads
+document.addEventListener('DOMContentLoaded', displayNotes)
